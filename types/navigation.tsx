55e4719ee8d522f45796c3f716023d9e10bab3ef
Jacob Mario Leonard P <160170771+jacobmario7@users.noreply.github.com>
2024-11-10 23:41:18 +0530

    Create navigation.tsx

---
+++ types/navigation.tsx
@@ -0,0 +1,9 @@
+
+export type RootStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  DoctorDashboard: undefined;
+  PatientDashboard: undefined;
+  PrescriptionUpload: undefined;
+  MedicationReminder: undefined;
+};
